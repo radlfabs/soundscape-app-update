a70539a3d62496547fdc042471fee3fac0989ea1
radlfabs <rosenthal.fabian@gmail.com>
2023-06-19 10:38:48 +0200

    update code for presentation ready app

--- soundscape_viz_app.py
+++ soundscape_viz_app.py
@@ -1,6 +1,11 @@
 # Author: Fabian Rosenthal
 # Use this module with a bokeh server to visualize the soundscape data.
-# Example usage from command line: bokeh serve --show soundscape_viz_app.py
+# Example usage from command line: 
+# bokeh serve --show soundscape_viz_app.py
+
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
 import numpy as np
 
@@ -9,28 +14,34 @@ from bokeh.models import CDSView
 from bokeh.models import BooleanFilter
 from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.models import Slider
+from bokeh.models import Div
 
-import src.preprocess as pp
-from src.constants import USEFUL_COLUMNS
-from src.constants import CATEGORICAL_CMAP
-
-from src.plot_functions import get_radar_chart
-from src.plot_functions import get_timeseries_plot
-from src.plot_functions import get_rel_plot
-from src.plot_functions import create_time_color_map
-from src.plot_functions import get_daily_observations_barplot
-from src.plot_functions import get_soundcat_barplot
-
-from src.make_divs import get_person_div
-from src.make_divs import title_div
-from src.make_divs import radar_desc_div
-from src.make_divs import slider_desc_div
-from src.make_divs import time_series_div
-from src.make_divs import loudness_div
-from src.make_divs import situation_div
-from src.make_divs import citation_div
-from src.make_divs import thk_div
+import preprocess as pp
+from constants import USEFUL_COLUMNS
+from constants import CATEGORICAL_CMAP
+from plot_functions import get_radar_chart
+from plot_functions import make_plot_dict
+from plot_functions import create_time_color_map
+from plot_functions import get_daily_observations_barplot
+from plot_functions import get_soundcat_barplot
+from make_divs import get_person_div
+from make_divs import get_person_text
+from make_divs import title_div
+from make_divs import radar_desc_div
+from make_divs import slider_desc_div
+from make_divs import time_series_div
+from make_divs import loudness_div
+from make_divs import situation_div
+from make_divs import environment_div
+from make_divs import citation_div
+from make_divs import thk_div
+from widgets import running_spinner
+from widgets import spinner_styles
+from widgets import get_slider
+from widgets import get_buttons
+from log_session import initialize_log
+from log_session import log_id
+from log_session import log_selection
 
 # load the data
 (
@@ -43,54 +54,46 @@ from src.make_divs import thk_div
 ) = pp.load_data()
 
 # set initial index and number of rows
-initial_idx = 0
+idx = 0
+initialize_log()
+log_id(distinct_df.iloc[idx]["ID"], is_marked=False)
 n_rows = pp.get_num_participants(distinct_df)
 
 # create data div
-person_data_div = get_person_div(distinct_df.iloc[initial_idx])
+person_data_div = get_person_div(distinct_df.iloc[idx])
 
-# create a view containing a filter corresponding to the initial index
-booleans = (df.index == initial_idx).tolist()
-cds_filter = BooleanFilter(booleans)
-view = CDSView(filter=cds_filter)
+# make the filters for the timeseries and relationship plots
+# then initialize the CDSView with the filter
+filters = {
+    i: BooleanFilter((df.index == i).tolist()) for i in range(n_rows)
+}
+view = CDSView(filter=filters[idx])
 
-# create columndatasources for the radar charts
-# and create the radar charts
-cds_health = ColumnDataSource(data=xy_data_dict["Wellbeing"][initial_idx])
-radar_health = get_radar_chart(*charts_data_dict["Wellbeing"][initial_idx], cds_health)
-
-cds_noise = ColumnDataSource(data=xy_data_dict["Noise Sensitivity"][initial_idx])
-radar_noise = get_radar_chart(
-    *charts_data_dict["Noise Sensitivity"][initial_idx], cds_noise
-)
-
-cds_trait = ColumnDataSource(data=xy_data_dict["Trait"][initial_idx])
-radar_trait = get_radar_chart(*charts_data_dict["Trait"][initial_idx], cds_trait)
+radar_charts = {
+    "Wellbeing": (
+        cds_health := ColumnDataSource(data=xy_data_dict["Wellbeing"][idx]),
+        get_radar_chart(*charts_data_dict["Wellbeing"][idx], cds_health)
+    ),
+    "Noise Sensitivity": (
+        cds_noise := ColumnDataSource(data=xy_data_dict["Noise Sensitivity"][idx]),
+        get_radar_chart(*charts_data_dict["Noise Sensitivity"][idx], cds_noise)
+    ),
+    "Trait": (
+        cds_trait := ColumnDataSource(data=xy_data_dict["Trait"][idx]),
+        get_radar_chart(*charts_data_dict["Trait"][idx], cds_trait)
+    ),
+}
 
 # Get the useful columns with respect to visualisation and create a ColumnDataSource for the timeseries
 cols = USEFUL_COLUMNS
 cds = ColumnDataSource(df.loc[:, [*cols, "Time"]])
-
-# create the timeseries and rel-plotsplots
-ev_plot = get_timeseries_plot(cds, view, "Soundscape_eventfulness")
-pl_plot = get_timeseries_plot(cds, view, "Soundscape_pleasantness")
-ev_pl_rel_plot = get_rel_plot(
-    cds, view, ["Soundscape_eventfulness", "Soundscape_pleasantness"]
-)
-perc_loud_plot = get_timeseries_plot(cds, view, "Perceived_loudness")
-pred_loud_plot = get_timeseries_plot(cds, view, "Predicted_Loudness")
-loudness_rel_plot = get_rel_plot(
-    cds, view, ["Perceived_loudness", "Predicted_Loudness"]
-)
-val_plot = get_timeseries_plot(cds, view, "Valence")
-ar_plot = get_timeseries_plot(cds, view, "Arousal")
-val_ar_rel_plot = get_rel_plot(cds, view, ["Valence", "Arousal"])
+plot_dict = make_plot_dict(cds, view)
 
 # create the barplots for the daily observations
 hours_colormap = create_time_color_map()
 x_counts = np.arange(24)
 cds_counts = ColumnDataSource(
-    {"x": x_counts, "counts": counts_array[initial_idx, :], "cmap": hours_colormap}
+    {"x": x_counts, "counts": counts_array[idx, :], "cmap": hours_colormap}
 )
 count_plot = get_daily_observations_barplot(cds_counts)
 
@@ -98,58 +101,67 @@ count_plot = get_daily_observations_barplot(cds_counts)
 x_cats = np.arange(8)
 cats_cmap = CATEGORICAL_CMAP
 cds_cats = ColumnDataSource(
-    {"x": x_cats, "categories": sound_category_sums[initial_idx, :], "cmap": cats_cmap}
+    {"x": x_cats, "categories": sound_category_sums[idx, :], "cmap": cats_cmap}
 )
 cat_plot = get_soundcat_barplot(cds_cats)
 
-# Create the slider widget that selects the participant via idx
-slider = Slider(
-    start=1, end=n_rows, value=initial_idx + 1, step=1, title="Select a participant:"
-)
-slider.styles = {
-    "width": "30%",
-    "position": "fixed",
-    "top": "0",
-    "right": "0",
-    "z-index": "1",
-    "background-color": "rgba(128, 128, 128, 0.5)",
-    "border-radius": "10px",
-    "padding": "10px",
-    "color": "white",
-}
+empty_spinner = Div(text="")
+spinner = empty_spinner
+running_spinner.styles = spinner_styles
+
+slider = get_slider(n_rows, idx)
 
 
-# Define the callback function for slider changes
-def update(attr, old, new):
+def update_slider(attr, old, new):
     # get the nessesary variables from the global scope
-    global df, idx, layout, view, cds_health, cds_noise, cds_trait, cds_counts, cds_cats
+    global df, idx, spinner, cds, view
 
-    # assign the new index and update the CDSview
-    # updating hte view will update all timeseries and relationship plots
+    # set the spinner to running
+    spinner = running_spinner
+    
+    # assign the new index and update the CDSview for all timeseries and relationship plots
     idx = slider.value - 1
-    view.filter = BooleanFilter((df.index == idx).tolist())
-
+    
+    # change the filter of the view
+    view.filter = filters[idx]
+    cds.selected.indices = []
     # make a new person div and replace the old one
-    layout.children[3] = get_person_div(distinct_df.iloc[idx])
+    person_data_div.text = get_person_text(distinct_df.iloc[idx])
 
-    # Update dta for the radar charts' titles and data
-    radar_health.title.text = charts_data_dict["Wellbeing"][idx][0]
-    cds_health.data = xy_data_dict["Wellbeing"][idx]
-
-    radar_noise.title.text = charts_data_dict["Noise Sensitivity"][idx][0]
-    cds_noise.data = xy_data_dict["Noise Sensitivity"][idx]
-
-    radar_trait.title.text = charts_data_dict["Trait"][idx][0]
-    cds_trait.data = xy_data_dict["Trait"][idx]
+    # update the radar charts
+    for radar_name, (source, radar_chart) in radar_charts.items():
+        # sets the new title
+        radar_chart.title.text = charts_data_dict[radar_name][idx][0]
+        # sets the new data
+        source.data = xy_data_dict[radar_name][idx]
 
     cds_counts.data = {**cds_counts.data, "counts": counts_array[idx, :]}
     cds_cats.data = {**cds_cats.data, "categories": sound_category_sums[idx, :]}
-
+    
+    # log the change
+    log_id(distinct_df.iloc[idx]["ID"], is_marked=False)
+    # empty the spinner
+    spinner = empty_spinner
 
 # Attach the callback to the slider widget
 slider.on_change(
-    "value_throttled", update
+    "value_throttled", update_slider
 )  # instead of on_change we want to only update if the slider is released. This is why we use value_throttled
+
+selected_value = 0
+# Callback function for tap event
+def update_selection(attr, old, new):
+    global selected_value
+    try:
+        selected_patch = cds.selected.indices[0]
+        selected_value = cds.data['Trigger_counter'][selected_patch]
+    except:
+        selected_value = "Indexing error"
+
+cds.selected.on_change("indices", update_selection)
+button_mark_id, button_mark_selection = get_buttons()
+button_mark_id.on_click(lambda event: log_id(distinct_df.iloc[idx]["ID"], is_marked=True))
+button_mark_selection.on_click(lambda event: log_selection(distinct_df.iloc[idx]["ID"], selected_value))
 
 # Create the layout and add the components
 layout = column(
@@ -157,24 +169,34 @@ layout = column(
     slider_desc_div,
     slider,
     person_data_div,
-    row(radar_health, radar_noise, radar_trait),
+    row(*[data[1] for data in radar_charts.values()]),
     radar_desc_div,
     count_plot,
     cat_plot,
+    
     time_series_div,
-    pl_plot,
-    ev_plot,
-    ev_pl_rel_plot,
+    plot_dict["pl_plot"],
+    plot_dict["ev_plot"],
+    plot_dict["ev_pl_rel_plot"],
+    
     loudness_div,
-    perc_loud_plot,
-    pred_loud_plot,
-    loudness_rel_plot,
+    plot_dict["perc_loud_plot"],
+    plot_dict["pred_loud_plot"],
+    plot_dict["loudness_rel_plot"],
+    
     situation_div,
-    val_plot,
-    ar_plot,
-    val_ar_rel_plot,
+    plot_dict["val_plot"],
+    plot_dict["ar_plot"],
+    plot_dict["val_ar_rel_plot"],
+
+    environment_div,
+    plot_dict["temp_plot"],
+    plot_dict["lumin_plot"],
+    
     citation_div,
     thk_div,
+    row(button_mark_id, button_mark_selection),
+    spinner,
 )
 
 # Add the layout to the current document (curdoc)
